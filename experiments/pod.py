262e7366fc725b99eef24ee38ffb364a4566c20d
Christian Møldrup Legaard <clegaard@outlook.com>
2021-11-15 15:22:39 +0100

    .

--- experiments/pod.py
+++ experiments/pod.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import full
 from numpy.lib.twodim_base import diag
 from datasets import heateq_2d_square
 from plotting import animate_2d_image
@@ -7,18 +8,30 @@ import numpy as np
 
 if __name__ == "__main__":
 
+    n_components = 2
+
     u, t, x, y = heateq_2d_square(t_start=0.0, t_end=1.0, Δt=0.01, x_start=0.0,
                                   x_end=1.0, Δx=0.01, y_start=0.0, y_end=1.0, Δy=0.01, α=-0.5)
 
-    # U, Σ, V = svd(u, full_matrices=True)
+    # reshape from (Nt x Nx x Ny) to (Nt x Nx*Ny)
+    u_flattened = u.reshape(t.shape[0], -1)
 
-    # ax.stem(Σ)
-    # ax.set_title("Singular values summed along time")
-    # u_reconstructed = U @ Σ @ V.T
+    U, Σ, V = svd(u_flattened, full_matrices=False)
+    u_reconstructed_full = np.dot(U * Σ, V).reshape(*u.shape)
+    u_reconstructed_truncated = np.dot(
+        U[:, :n_components] * Σ[:n_components], V[:n_components, :]).reshape(*u.shape)
+    assert u_reconstructed_full.shape == u_reconstructed_truncated.shape, "reconstrtion of full SVD and truncated SVD should have identical dimensions"
 
-    # fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
+    ax.stem(Σ)
 
     fig1, ax1, ani1 = animate_2d_image(u, t)
-    # fig2, ax2, ani2 = animate_2d_image(u_reconstructed, t)
+    ax1.set_title("original u(x,y,t)")
+
+    fig2, ax2, ani2 = animate_2d_image(u_reconstructed_full, t)
+    ax2.set_title("reconstructed u(x,y,t)")
+
+    fig3, ax3, ani3 = animate_2d_image(u_reconstructed_truncated, t)
+    ax3.set_title(f"reconstructed truncated u(x,y,t), N={n_components}")
 
     plt.show()
--- questions.md
+++ questions.md
@@ -10,18 +10,16 @@ dudy = np.gradient(cur,Δy,axis=1)
 dudyy = np.gradient(dudy, axis=1)
 ```
 
-# temporal averaging
-
-I would expect that U = [u0 u1 u2 ... un], where u_k are (N x M) snapshots of the system.
-Wikipedia suggests that the signals are averaged for every point in space:
-https://en.wikipedia.org/wiki/Proper_orthogonal_decomposition
-
-I would expect the method to work like:
-U(t, u0) = [z0, z1 z2 ... zm] * ϕ(t)
-
-where z_i represents a 2-dimensional stencil that is modulated over time by ϕ(t) to reconstruct an component of the signal.
-
 
 # Resources
 http://web.mit.edu/6.242/www/images/lec6_6242_2004.pdf
 https://levelup.gitconnected.com/solving-2d-heat-equation-numerically-using-python-3334004aa01a
+
+
+# Implement Implicit scheme
+
+Define Diffusion operator K.
+This would require a stencil like 1 -2 1 for the 1D case
+
+# Implement Galerkin projection
+
