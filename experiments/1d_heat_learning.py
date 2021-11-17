47f10a49c681bdcfb661d0e82032c1256f4f645d
Christian Legaard <clegaard@outlook.com>
2021-11-17 11:07:48 +0100

    Finish full order explict scheme

--- experiments/1d_heat_learning.py
+++ experiments/1d_heat_learning.py
@@ -6,11 +6,12 @@ import torch.nn.functional as F
 from datasets import heateq_1d_square_implicit_euler_matrix
 from plotting import heatmap_1d
 import matplotlib.pyplot as plt
+from torch.nn.init import xavier_uniform_
 
 if __name__ == "__main__":
 
-    n_train_iterations = 10000
-    n_train_steps = 20
+    n_train_iterations = 1000
+    n_train_steps = 149
     device = "cuda"
 
     Δt = 0.01
@@ -24,6 +25,10 @@ if __name__ == "__main__":
         α=-0.5,
     )
 
+    assert n_train_steps <= len(
+        t
+    ), f"training data must have a length greater or equal than the number of training steps, actual is {len(t)} and {n_train_steps}"
+
     # TODO
     u = u.T
     # end TODO
@@ -33,51 +38,55 @@ if __name__ == "__main__":
     t = t[: n_train_steps + 1]
     u0 = u[:, 0].to(device)
 
-    # M = torch.randn(M.shape, requires_grad=True).to(device)
-    M = torch.nn.Linear(M.shape[0], M.shape[0]).to(device)
+    M = torch.nn.Linear(M.shape[0], M.shape[0], bias=False).to(device)
+    xavier_uniform_(M.weight)
     loss = None
     losses = []
     opt = torch.optim.Adam(M.parameters())
 
     # ---------------------- foward euler explicit, full system ----------------------------------
-    u_estimated = None
 
     for _ in tqdm(range(n_train_iterations), desc="training iteration"):
-        cur = u0
-        next = None
-        uu = [u0]
-
-        for _ in range(n_train_steps):
-            next = cur + M(cur) * Δt
-            uu.append(next)
-            cur = next
-
-        u_estimated = torch.stack(uu, axis=1)
-        loss = F.mse_loss(u_estimated, u)
+        # map each snapshot x_k to an estimate of the next state x_k+1
+        # operation can be carried out in parallel for each snapshot
+        u_next = u + M(u.T).T * Δt
+        # u = [x0 x1 ... xn] , u_next = [x1 x2 ... xn+1]
+        loss = F.mse_loss(u_next[:, :-1], u[:, 1:])
         loss.backward()
         losses.append(loss.item())
         opt.step()
         opt.zero_grad()
 
-    # u_step = u.unfold(dimension=1, size=1, step=1)
-    # for _ in tqdm(range(n_train_iterations), desc="training iteration"):
-    #     next = u_step + M(u_step) * Δt
-    #     u_estimated_step = u_estimated.unfold(dimension=1, size=1, step=1)
+    # ================================ validation ==============================
+    u_estimated_step = torch.concat((u[:, :1], u_next[:, :-1]), dim=1)
 
-    #     loss = F.mse_loss(u_estimated, u)
-    #     loss.backward()
-    #     losses.append(loss.item())
-    #     opt.step()
-    #     opt.zero_grad()
+    cur = u[:, :1]
+    next = None
+    us = [cur]
+    for _ in tqdm(t):
+        next = cur + M(cur.T).T * Δt
+        us.append(next)
+        cur = next
+    u_estimated_sim = torch.concat(us, dim=1)
+    assert u_estimated_step.shape == u_estimated_sim.shape
 
     # ================================ plotting ================================
 
-    u_estimated = u_estimated.detach().cpu().numpy()
+    u = u.detach().cpu().numpy()
+    u_estimated_step = u_estimated_step.detach().cpu().numpy()
+    u_estimated_sim = u_estimated_sim.detach().cpu().numpy()
 
     fig, ax = plt.subplots()
     ax.plot(losses)
     ax.set_xlabel("training step")
-    ax.set_ylabel("loss")
+    ax.set_ylabel("loss, single step")
 
-    heatmap_1d(u_estimated.T, x, t)
+    fig, ax = heatmap_1d(u.T, x, t)
+    ax.set_title("true")
+
+    fig, ax = heatmap_1d(u_estimated_step.T, x, t)
+    ax.set_title("predicted (step)")
+
+    fig, ax = heatmap_1d(u_estimated_sim.T, x, t)
+    ax.set_title("predicted (sim)")
     plt.show()
