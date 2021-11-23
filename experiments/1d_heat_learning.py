bc0192d41ef9dbb2ca4bdaaf39741a0c115b51b4
Christian Legaard <clegaard@outlook.com>
2021-11-23 16:44:45 +0100

    Refactor code

--- experiments/1d_heat_learning.py
+++ experiments/1d_heat_learning.py
@@ -5,289 +5,315 @@ from datasets import heateq_1d_square_implicit_euler_matrix
 from plotting import heatmap_1d
 import matplotlib.pyplot as plt
 from torch.nn.init import xavier_uniform_
+import scipy
+from einops import rearrange
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+
+def discrete_laplacian_1d(n, dtype):
+    K = torch.zeros((n, n), dtype=dtype)
+    diag = torch.tensor([1.0, -2.0, 1.0], dtype=dtype)
+    K[0, 0:3] = diag
+    K[-1, -3:] = diag
+    for i in range(1, K.shape[0] - 1):
+        K[i, i - 1 : i + 2] = diag
+
+    return K
+
+
+def lu_inverse(x):
+    pl, u = scipy.linalg.lu(x, permute_l=True)
+    pli = torch.linalg.inv(torch.tensor(pl))
+    ui = torch.linalg.inv(torch.tensor(u))
+
+    return pli, ui
+
+
+def batch_mult(K, X):
+    return torch.matmul(X[:, None, :, :].T, K.T).T.squeeze(1)
+
+
+def train_model(x, γ, n_train_iterations, mode, regularize_diagonal, regularize_l1):
+    K = torch.empty(
+        (x.shape[0], x.shape[0]),
+        dtype=dtype,
+        device=device,
+        requires_grad=True,
+    )
+    xavier_uniform_(K)
+    opt = torch.optim.Adam([K], lr=lr)
+
+    scheduler = ReduceLROnPlateau(opt, "min")
+    losses = []
+
+    def K_func():
+        if regularize_diagonal:
+            return -K.T @ K
+        else:
+            return K
+
+    for _ in tqdm(range(n_train_iterations), desc="training explicit"):
+
+        if mode == "explicit":
+            x_next = x + batch_mult(K_func() * γ, x)
+            loss = F.mse_loss(x_next[:, :-1], x[:, 1:])
+        elif mode == "implicit":
+            x_prev = x - batch_mult(K_func() * γ, x)
+            loss = F.mse_loss(x_prev[:, 1:], x[:, :-1])
+        else:
+            raise ValueError("invalid value for mode")
+
+        if regularize_l1:
+            loss = loss + 0.000001 * torch.norm(K, p=1)
+
+        loss.backward()
+        losses.append(loss.item())
+        opt.step()
+        opt.zero_grad()
+        scheduler.step(loss)
+
+    return K_func(), losses
+
+
+def simulate(M, t, x0):
+    next = None
+    x = [x0]
+    cur = x0
+    for _ in range(len(t) - 1):
+        next = batch_mult(M, cur)
+        x.append(next)
+        cur = next
+    x = torch.concat(x, dim=1)
+    return x
+
 
 if __name__ == "__main__":
 
     n_train_iterations = 30000
+    n_experiments_train = 1
+    n_experiments_validate = 0
     n_modes = 10
     device = "cuda"
-    dtype = torch.float64
+    dtype = torch.float32
 
     Δt = 0.01
     α = 0.005
     Δx = 0.01
     lr = 1e-3
-    γ = α * Δt / Δx ** 2
-    u, t, x, K = heateq_1d_square_implicit_euler_matrix(
-        t_start=0.0,
-        t_end=2.0,
-        Δt=Δt,
-        x_start=0.0,
-        x_end=1.0,
-        Δx=Δx,
-        α=α,
-    )
+    t_start = 0.0
+    t_end = 2.0
+    x_start = 0.0
+    x_end = 1.0
+    regularize_diagonal = True
+
+    # u, t, x, K = heateq_1d_square_implicit_euler_matrix(
+    #     t_start=0.0,
+    #     t_end=2.0,
+    #     Δt=Δt,
+    #     x_start=0.0,
+    #     x_end=1.0,
+    #     Δx=Δx,
+    #     α=α,
+    # )
 
     # ===================== Generate Data ==============================
 
-    #
+    t = torch.arange(t_start, t_end, Δt, dtype=dtype)
+    x = torch.arange(x_start, x_end, Δx, dtype=dtype)
 
-    # TODO
-    u = u.T
-    # end TODO
+    n_steps = t.shape[0]
 
-    u = torch.tensor(u, dtype=dtype).to(device)
+    u0 = torch.rand(
+        (*x.shape, n_experiments_train + n_experiments_validate), dtype=dtype
+    )
+    u = [u0]
+    cur = u0
 
-    u0 = u[:, 0].to(device)
+    K = discrete_laplacian_1d(x.shape[0], dtype)
+    γ = α * Δt / Δx ** 2
+    M = torch.eye(K.shape[0]) - K * γ
+
+    li, ui = lu_inverse(M)
+
+    for _ in tqdm(range(n_steps - 1), desc="stepping"):
+        next = ui @ li @ cur
+        u.append(next)
+        cur = next
+
+    u = torch.stack(u, dim=1).to(device)  # Nx x Nt x Ne
+    u_flat = rearrange(u, "nx nt ne -> nx (nt ne)")
 
     # ================== Dimensionality Reduction ======================
-    U, S, V = torch.linalg.svd(u, full_matrices=False)
+    U, S, V = torch.linalg.svd(u_flat, full_matrices=False)
 
-    z = U.T @ u
-    u_reconstructed_full = U @ z
+    z = batch_mult(U.T, u)
+    u_reconstructed_full = batch_mult(U, z)
     Ut = U[:, :n_modes]
-    zt = Ut.T @ u
-    u_reconstructed_truncated = Ut @ zt
+    zt = batch_mult(Ut.T, u)
 
+    u_reconstructed_truncated = batch_mult(Ut, zt)
     K_reduced = Ut.T.cpu() @ K @ Ut.cpu()
 
     # ========================== Training =============================
 
-    results = {
-        "full": {
-            "explicit": {"losses": [], "sim": None, "step": None},
-            "implicit": {"losses": [], "sim": None, "step": None},
-        },
-        "reduced": {
-            "explicit": {"losses": [], "sim": None, "step": None},
-            "implicit": {"losses": [], "sim": None, "step": None},
-        },
-    }
-
     # =========== full system -> forward euler ===========
 
-    K_explicit = torch.empty(K.shape, dtype=dtype, device=device, requires_grad=True)
-    xavier_uniform_(K_explicit)
-    opt = torch.optim.Adam([K_explicit], lr=lr)
-
-    for _ in tqdm(range(n_train_iterations), desc="training explicit"):
-        u_next = u + K_explicit @ u * γ
-        loss = F.mse_loss(u_next[:, :-1], u[:, 1:])
-        loss.backward()
-        results["full"]["explicit"]["losses"].append(loss.item())
-        opt.step()
-        opt.zero_grad()
-
-    results["full"]["explicit"]["step"] = torch.concat(
-        (u[:, :1], u_next[:, :-1]), dim=1
-    )
-    cur = u[:, :1]
-    next = None
-    us = [cur]
-    for _ in t:
-        next = cur + K_explicit @ cur * γ
-        us.append(next)
-        cur = next
-    results["full"]["explicit"]["sim"] = torch.concat(us, dim=1)
-
-    assert (
-        results["full"]["explicit"]["step"].shape
-        == results["full"]["explicit"]["sim"].shape
+    K_full_explicit, losses_full_explicit = train_model(
+        u, γ, n_train_iterations, "explicit", True, regularize_l1=False
     )
 
-    results["full"]["explicit"]["step"] = (
-        results["full"]["explicit"]["step"].detach().cpu().numpy()
+    M_full_explicit = (
+        torch.eye(K_full_explicit.shape[0], device=device) + K_full_explicit * γ
     )
-    results["full"]["explicit"]["sim"] = (
-        results["full"]["explicit"]["sim"].detach().cpu().numpy()
-    )
-    K_explicit = K_explicit.detach().cpu().numpy()
+
+    x_full_explicit = simulate(M_full_explicit, t, x0=u[:, :1, :])
+
+    x_full_explicit = x_full_explicit.detach().cpu()
+    K_full_explicit = K_full_explicit.detach().cpu()
 
     # =========== full system -> backward euler ===========
 
-    K_implicit = torch.empty(K.shape, dtype=dtype, device=device, requires_grad=True)
-    xavier_uniform_(K_implicit)
-    opt = torch.optim.Adam([K_implicit], lr=lr)
-
-    for _ in tqdm(range(n_train_iterations), desc="training implicit"):
-        u_prev = u - K_implicit @ u * γ
-        loss = F.mse_loss(u_prev[:, 1:], u[:, :-1])
-        loss.backward()
-        results["full"]["implicit"]["losses"].append(loss.item())
-        opt.step()
-        opt.zero_grad()
-
-    results["full"]["implicit"]["step"] = torch.concat(
-        (u_prev[:, 1:], u[:, -1:]), dim=1
+    K_full_implicit, losses_full_implicit = train_model(
+        u,
+        γ,
+        n_train_iterations,
+        "implicit",
+        regularize_diagonal=regularize_diagonal,
+        regularize_l1=False,
     )
 
-    M_implicit_inv = torch.linalg.inv(
-        torch.eye(K_implicit.shape[0]).to(device) - K_implicit * γ
-    )
-    cur = u[:, :1]
-    next = None
-    us = [cur]
-    for _ in t:
-        next = M_implicit_inv @ cur
-        us.append(next)
-        cur = next
-    results["full"]["implicit"]["sim"] = torch.concat(us, dim=1)
-
-    assert (
-        results["full"]["implicit"]["step"].shape
-        == results["full"]["implicit"]["sim"].shape
+    M_full_implicit = torch.linalg.inv(
+        torch.eye(K_full_implicit.shape[0], device=device) - K_full_implicit * γ
     )
 
-    results["full"]["implicit"]["step"] = (
-        results["full"]["implicit"]["step"].detach().cpu().numpy()
-    )
-    results["full"]["implicit"]["sim"] = (
-        results["full"]["implicit"]["sim"].detach().cpu().numpy()
-    )
+    x_full_implicit = simulate(M_full_implicit, t, u[:, :1])
 
-    K_implicit = K_implicit.detach().cpu().numpy()
+    K_full_implicit = K_full_implicit.detach().cpu()
+    x_full_implicit = x_full_implicit.detach().cpu()
 
     # =========== reduced system -> forward euler ===========
 
-    K_reduced_explicit = torch.empty(
-        (n_modes, n_modes), dtype=dtype, device=device, requires_grad=True
-    )
-    xavier_uniform_(K_reduced_explicit)
-    opt = torch.optim.Adam([K_reduced_explicit], lr=lr)
-
-    for _ in tqdm(range(n_train_iterations), desc="training reduced explicit"):
-        z_next = zt + K_reduced_explicit @ zt * γ
-        loss = F.mse_loss(z_next[:, :-1], zt[:, 1:])
-        loss.backward()
-        results["reduced"]["explicit"]["losses"].append(loss.item())
-        opt.step()
-        opt.zero_grad()
-
-    zs_step = torch.concat((zt[:, :1], z_next[:, :-1]), dim=1)
-    results["reduced"]["explicit"]["step"] = Ut @ zs_step
-    cur = zt[:, :1]
-    next = None
-    zs = [cur]
-    for _ in t:
-        next = cur + K_reduced_explicit @ cur * γ
-        zs.append(next)
-        cur = next
-
-    zs = torch.concat(zs, dim=1)
-    results["reduced"]["explicit"]["sim"] = Ut @ zs
-
-    assert (
-        results["reduced"]["explicit"]["step"].shape
-        == results["reduced"]["explicit"]["sim"].shape
+    K_reduced_explicit, losses_reduced_explicit = train_model(
+        zt,
+        γ,
+        n_train_iterations,
+        "explicit",
+        regularize_diagonal=regularize_diagonal,
+        regularize_l1=False,
     )
 
-    results["reduced"]["explicit"]["step"] = (
-        results["reduced"]["explicit"]["step"].detach().cpu().numpy()
+    M_reduced_explicit = (
+        torch.eye(K_reduced_explicit.shape[0], device=device) + K_reduced_explicit * γ
     )
-    results["reduced"]["explicit"]["sim"] = (
-        results["reduced"]["explicit"]["sim"].detach().cpu().numpy()
-    )
-    K_reduced_explicit = K_reduced_explicit.detach().cpu().numpy()
+
+    zs = simulate(M_reduced_explicit, t, zt[:, :1])
+    x_reduced_explicit = batch_mult(Ut, zs)
+
+    x_reduced_explicit = x_reduced_explicit.detach().cpu()
+    K_reduced_explicit = K_reduced_explicit.detach().cpu()
 
     # ============= Reduced system -> Backward euler =================
 
-    K_reduced_implicit = torch.empty(
-        (n_modes, n_modes), dtype=dtype, device=device, requires_grad=True
+    K_reduced_implicit, losses_reduced_implicit = train_model(
+        zt,
+        γ,
+        n_train_iterations,
+        "implicit",
+        regularize_diagonal=False,
+        regularize_l1=False,
     )
-    xavier_uniform_(K_reduced_implicit)
-    opt = torch.optim.Adam([K_reduced_implicit], lr=lr)
-
-    for _ in tqdm(range(n_train_iterations), desc="training reduced implicit"):
-        z_prev = zt - K_reduced_implicit @ zt * γ
-        loss = F.mse_loss(z_prev[:, 1:], zt[:, :-1])
-        loss.backward()
-        results["reduced"]["implicit"]["losses"].append(loss.item())
-        opt.step()
-        opt.zero_grad()
-
-    zs_step = torch.concat((z_prev[:, 1:], zt[:, -1:]), dim=1)
-    results["reduced"]["implicit"]["step"] = Ut @ zs_step
 
     M_reduced_implicit_inv = torch.linalg.inv(
-        torch.eye(K_reduced_implicit.shape[0]).to(device) - γ * K_reduced_implicit
-    )
-    cur = zt[:, :1]
-    next = None
-    zs = [cur]
-    for _ in t:
-        next = M_reduced_implicit_inv @ cur
-        zs.append(next)
-        cur = next
-    zs = torch.concat(zs, dim=1)
-    results["reduced"]["implicit"]["sim"] = Ut @ zs
-
-    assert (
-        results["reduced"]["implicit"]["step"].shape
-        == results["reduced"]["implicit"]["sim"].shape
+        torch.eye(K_reduced_implicit.shape[0], device=device) - K_reduced_implicit * γ
     )
 
-    results["reduced"]["implicit"]["step"] = (
-        results["reduced"]["implicit"]["step"].detach().cpu().numpy()
-    )
-    results["reduced"]["implicit"]["sim"] = (
-        results["reduced"]["implicit"]["sim"].detach().cpu().numpy()
-    )
+    zs = simulate(M_reduced_implicit_inv, t, zt[:, :1])
+    x_reduced_implicit = batch_mult(Ut, zs)
 
-    K_reduced_implicit = K_reduced_implicit.detach().cpu().numpy()
+    K_reduced_implicit = K_reduced_implicit.detach().cpu()
+    x_reduced_implicit = x_reduced_implicit.detach().cpu()
 
     # ================================ plotting ================================
-
-    u = u.detach().cpu().numpy()
-    u_reconstructed_full = u_reconstructed_full.detach().cpu().numpy()
-    u_reconstructed_truncated = u_reconstructed_truncated.detach().cpu().numpy()
+    plot_idx = 0
+    u = u.detach().cpu()
+    u_reconstructed_full = u_reconstructed_full.detach().cpu()
+    u_reconstructed_truncated = u_reconstructed_truncated.detach().cpu()
 
     fig, ax = plt.subplots()
     ax.set_title("Losses explicit")
-    ax.plot(results["full"]["explicit"]["losses"], label="full explicit")
-    ax.plot(results["full"]["implicit"]["losses"], label="full implicit")
-    ax.plot(results["reduced"]["explicit"]["losses"], label="reduced explicit")
-    ax.plot(results["reduced"]["implicit"]["losses"], label="reduced implicit")
+    ax.plot(losses_full_explicit, label="full explicit")
+    ax.plot(losses_full_implicit, label="full implicit")
+    ax.plot(losses_reduced_explicit, label="reduced explicit")
+    ax.plot(losses_reduced_implicit, label="reduced implicit")
     ax.set_xlabel("training step")
     ax.set_ylabel("loss, single step")
     ax.set_yscale("log")
     ax.legend()
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, sharey=True)
-    ax1.imshow(u, cmap="jet", origin="lower")
-    ax1.set_title("true")
-    ax2.imshow(results["full"]["explicit"]["sim"], cmap="jet", origin="lower")
-    ax2.set_title("explicit")
-    im = ax3.imshow(results["full"]["implicit"]["sim"], cmap="jet", origin="lower")
-    ax3.set_title("implicit ")
-    ax3.set_xlabel("x")
-    ax3.set_ylabel("t")
-    # plt.colorbar(im)
+    def plot_heatmap(name_to_data: dict, plot_idx: int, vmin, vmax):
+        fig, axes = plt.subplots(len(name_to_data), 1, sharex=True, sharey=True)
+        # combined = torch.cat([v[..., plot_idx] for v in name_to_data.values()])
+        # vmin = combined.min()
+        # vmax = combined.max()
+        for ax, (name, data) in zip(axes, name_to_data.items()):
+            ax.imshow(
+                data[..., plot_idx], vmin=vmin, vmax=vmax, cmap="jet", origin="lower"
+            )
+            ax.set_title(name)
+        axes[-1].set_xlabel("t")
+        axes[-1].set_ylabel("x")
+        return fig, axes
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, sharey=True)
-    ax1.imshow(u_reconstructed_truncated, cmap="jet", origin="lower")
-    ax1.set_title(f"true, reduced N={n_modes}")
-    ax2.imshow(results["reduced"]["explicit"]["sim"], cmap="jet", origin="lower")
-    ax2.set_title("explicit")
-    im = ax3.imshow(results["reduced"]["implicit"]["sim"], cmap="jet", origin="lower")
-    ax3.set_title("implicit")
-    ax3.set_xlabel("x")
-    ax3.set_ylabel("t")
-    # plt.colorbar(im)
+    vmin = u.min()
+    vmax = u.max()
+    fig, axes = plot_heatmap(
+        {
+            f"true full": u,
+            f"full explicit": x_full_explicit,
+            f"full implicit": x_full_implicit,
+            f"true reduced N={n_modes}": u_reconstructed_truncated,
+            f"reduced explicit": x_reduced_explicit,
+            f"reduced implicit": x_reduced_implicit,
+        },
+        plot_idx,
+        vmin,
+        vmax,
+    )
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.imshow(K)
+    vmin = torch.cat((K, K_full_explicit, K_full_implicit)).min()
+    vmax = torch.cat((K, K_full_explicit, K_full_implicit)).max()
+    ax1.imshow(K, vmin=vmin, vmax=vmax)
     ax1.set_title("K implicit")
-    ax2.imshow(K_explicit)
-    ax3.set_title("K estimated explicit")
-    ax3.imshow(K_implicit)
-    ax2.set_title("K estimated implicit")
+    ax2.imshow(K_full_explicit, vmin=vmin, vmax=vmax)
+    ax2.set_title("K estimated explicit")
+    ax3.imshow(K_full_implicit, vmin=vmin, vmax=vmax)
+    ax3.set_title("K estimated implicit")
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.imshow(K_reduced)
+    vmin = torch.cat((K_reduced, K_reduced_explicit, K_reduced_implicit)).min()
+    vmax = torch.cat((K_reduced, K_reduced_explicit, K_reduced_implicit)).max()
+    ax1.imshow(K_reduced, vmin=vmin, vmax=vmax)
     ax1.set_title("K reduced implicit")
-    ax2.imshow(K_reduced_explicit)
-    ax3.set_title("K reduced estimated explicit")
-    ax3.imshow(K_reduced_implicit)
-    ax2.set_title("K reduced estimated implicit")
+    ax2.imshow(K_reduced_explicit, vmin=vmin, vmax=vmax)
+    ax2.set_title("K reduced estimated explicit")
+    ax3.imshow(K_reduced_implicit, vmin=vmin, vmax=vmax)
+    ax3.set_title("K reduced estimated implicit")
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    ax1.plot(torch.linalg.eigvals(K).abs().sort(descending=True)[0])
+    ax1.set_title("K full eigenvalues")
+    ax2.plot(torch.linalg.eigvals(K_full_explicit).abs().sort(descending=True)[0])
+    ax2.set_title("K full explicit eigenvalues")
+    ax3.plot(torch.linalg.eigvals(K_full_implicit).abs().sort(descending=True)[0])
+    ax3.set_title("K full implicit eigenvalues")
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    ax1.plot(torch.linalg.eigvals(K_reduced).abs().sort(descending=True)[0])
+    ax1.set_title("K reduced eigenvalues")
+    ax2.plot(torch.linalg.eigvals(K_reduced_explicit).abs().sort(descending=True)[0])
+    ax2.set_title("K reduced explicit eigenvalues")
+    ax3.plot(torch.linalg.eigvals(K_reduced_implicit).abs().sort(descending=True)[0])
+    ax3.set_title("K full implicit eigenvalues")
 
     plt.show()
