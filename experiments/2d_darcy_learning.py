676e2abb1c997b30c6b34f910f3257929563a359
Christian Legaard <clegaard@outlook.com>
2021-11-25 17:36:48 +0100

    Update

--- experiments/2d_darcy_learning.py
+++ experiments/2d_darcy_learning.py
@@ -18,7 +18,43 @@ import torch.nn.functional as F
 from tqdm import tqdm
 
 
-def train(pressures, rhs, n_iterations, estimate_K_on_rhs):
+def train_seperable(pressures, rhs, n_iterations, estimation_mode, permeabilities, Ks):
+    # def make_K():
+    #     K = torch.randn(
+    #         pressures.shape[0],
+    #         pressures.shape[0],
+    #         dtype=pressures.dtype,
+    #         device=pressures.device,
+    #         requires_grad=True,
+    #     )
+    #     xavier_uniform_(K)
+    #     return K
+
+    # Ks = [make_K() for _ in permeabilities]
+
+    # optimizer = torch.optim.Adam(Ks, lr=1e-4)
+    losses = []
+    for _ in tqdm(range(n_iterations), desc="training step"):
+
+        if estimation_mode == "lhs":
+            K_total = torch.sum(torch.stack(Ks, dim=-1) * permeabilities, dim=-1)
+            pressures_estimated = torch.linalg.solve(K_total, rhs)
+        elif estimation_mode == "rhs":
+            pass
+        else:
+            raise ValueError("Unrecognized estimation mode")
+
+        loss = F.mse_loss(pressures_estimated, pressures)
+        # loss.backward()
+        # optimizer.step()
+        # optimizer.zero_grad()
+
+        losses.append(loss.item())
+
+    return K_total, losses
+
+
+def train(pressures, rhs, n_iterations, estimation_mode):
 
     K = torch.randn(
         pressures.shape[0],
@@ -33,10 +69,13 @@ def train(pressures, rhs, n_iterations, estimate_K_on_rhs):
     losses = []
     for iter in tqdm(range(n_iterations), desc="training step"):
 
-        if estimate_K_on_rhs:
+        if estimation_mode == "rhs":
             pressures_estimated = K @ rhs
-        else:
+        elif estimation_mode == "lhs":
             pressures_estimated = torch.linalg.solve(K, rhs)
+        else:
+            raise ValueError("Unrecognized estimation mode")
+
         loss = F.mse_loss(pressures_estimated, pressures)
         loss.backward()
         optimizer.step()
@@ -48,44 +87,51 @@ def train(pressures, rhs, n_iterations, estimate_K_on_rhs):
     return K, losses
 
 
-# def solve(points, Ks, rhs, permeabilities):
-#     pressures = torch.zeros_like(points)
-#     K_total = torch.zeros_like(points.shape[0], points.shape[0])
-
-#     for K, perm in zip(Ks, permeabilities):
-#         K_total += K * perm
-
-#     pressures = torch.linalg.solve(K_total, rhs)
-
-#     return pressures
-
 if __name__ == "__main__":
 
-    n_training_iterations = 100000
-    estimate_K_on_rhs = (
-        True  # estimate K matrix on rhs, p = Kf, otherwise estimate on lhs and invert
-    )
+    n_training_iterations = 10000
+    estimation_mode = "rhs"
 
     # =================== load data ===================
 
     mat = scipy.io.loadmat("matlab_files/pressure.mat")
+    permeabilities = torch.tensor(mat["permeability"]).reshape(-1)
     p_coarse = torch.tensor(mat["Pressure_coarse"])
     xy_coarse = torch.tensor(mat["CCoord"])
     rhs_coarse = torch.tensor(mat["rhs_coarse"])
     rhs_fine = torch.tensor(mat["rhs_fine"])
+    Kc = [torch.tensor(k[0]) for k in mat["Kc"]]
+    Kf = [torch.tensor(k[0]) for k in mat["Kf"]]
 
     p_fine = torch.tensor(mat["Pressure_fine"])
     xy_fine = torch.tensor(mat["FCoord"])
 
+    # TODO
+    # K_total = torch.zeros_like(Kc[0])
+    # for k, p in zip(Kc, permeabilities):
+    #     K_total += k * p
+    # plt.imshow(K_total)
+    # plt.show()
+    # pressures_estimated = torch.linalg.solve(
+    #     K_total, rhs_coarse
+    # )  # impossible to invert since diagonal is 0
+
     # =================== train ===================
 
     K, losses = train(
-        p_coarse, rhs_coarse, n_training_iterations, estimate_K_on_rhs=estimate_K_on_rhs
+        p_coarse, rhs_coarse, n_training_iterations, estimation_mode=estimation_mode
     )
-    if estimate_K_on_rhs:
+    # K, losses = train_seperable(
+    #     p_coarse, rhs_coarse, n_training_iterations, "lhs", permeabilities, Kc
+    # )
+
+    if estimation_mode == "rhs":
         p_coarse_estimated = (K @ rhs_coarse).detach().cpu()
-    else:
+    elif estimation_mode == "lhs":
         p_coarse_estimated = torch.linalg.solve(K, rhs_coarse).detach().cpu()
+    else:
+        raise ValueError()
+
     K = K.detach().cpu()
 
     # =================== plotting ===================
