54c5fd952de1486a54e56a816f5f202a4e556b9f
Christian Legaard <clegaard@outlook.com>
2021-12-01 14:58:02 +0100

    Add dimensionality reduction

--- experiments/2d_darcy_learning.py
+++ experiments/2d_darcy_learning.py
@@ -1,20 +1,11 @@
 import scipy.io
 import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
 import torch
 import torch.linalg
 from torch.nn.init import xavier_uniform_
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-
-
-def create_model(hc):
-    pass
-
-
-# def assemble_k_matrices(points, connectivity)
-
+import numpy as np
 import torch.nn.functional as F
-
 from tqdm import tqdm
 from pyDOE import lhs
 
@@ -55,7 +46,7 @@ def train_seperable(pressures, rhs, n_iterations, estimation_mode, permeabilitie
     return K_total, losses
 
 
-def train(pressures, rhs, n_iterations, estimation_mode):
+def train(pressures, rhs, n_iterations, estimation_mode, lr, lr_schedule):
 
     K = torch.randn(
         pressures.shape[0],
@@ -65,8 +56,8 @@ def train(pressures, rhs, n_iterations, estimation_mode):
         requires_grad=True,
     )
     xavier_uniform_(K)
-    optimizer = torch.optim.Adam([K], lr=1e-4)
-    scheduler = ReduceLROnPlateau(optimizer, mode="min", patience=100)
+    optimizer = torch.optim.Adam([K], lr=lr)
+    scheduler = ReduceLROnPlateau(optimizer, mode="min")
 
     rhs = rhs.repeat(1, pressures.shape[-1])
     losses = []
@@ -86,29 +77,11 @@ def train(pressures, rhs, n_iterations, estimation_mode):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        if iter > 10000:
+        if lr_schedule:
             scheduler.step(loss)
         losses.append(loss.item())
 
     return K, losses
-
-
-# function Pressure = DarcyBlockSolver(P,Permeability)
-
-# Pressure = zeros(size(P.K{1},1),size(Permeability,1));
-
-# for k=1:size(Permeability,1)
-#      K=Permeability(k,1).*P.K{1};
-#      for j= 2:9
-#      K= K+Permeability(k,j).*P.K{j};
-#      end
-#     %rhs= Permeability(k,4)*P.rhs; % constant pressure gradient
-#     rhs= P.rhs; % constant inlet velocity
-#     Pressure(:,k) = K\rhs;
-# end
-
-
-# end
 
 
 def darcy_block_solver(Ks, permeabilities, rhs):
@@ -119,7 +92,7 @@ def darcy_block_solver(Ks, permeabilities, rhs):
         K_total = K_total.sum(dim=-1)
         rhss = rhs.squeeze(-1)
         pressures = torch.linalg.solve(K_total.T, rhss).T
-        return pressures
+        return K_total, pressures
 
     def looped():
         pressures = []
@@ -130,26 +103,22 @@ def darcy_block_solver(Ks, permeabilities, rhs):
             pressures.append(torch.linalg.solve(K_total.to_dense(), rhs))
 
         pressures = torch.cat(pressures, dim=-1)
-        return pressures
+        return K_total, pressures
 
     # pressures_b = batched()
     # pressures_l = looped()
     # assert pressures_l.allclose(pressures_b)
 
-    pressures = looped()
+    K, pressures = looped()
 
-    return pressures
+    return K, pressures
 
 
 def scipy_sparse_to_torch_sparse(csr):
-    import numpy as np
 
     coo = csr.tocoo()
-
     values = coo.data
     indices = np.vstack((coo.row, coo.col))
-    # i = torch.LongTensor(indices)
-    # v = torch.FloatTensor(values)
 
     s = torch.sparse_coo_tensor(indices, values, torch.Size(coo.shape))
     return s
@@ -176,11 +145,23 @@ def add_outlet_to_solution(p_without_outlet, xy_coords):
     return p_with_outlet
 
 
+def batch_mult(K, X):
+    return torch.matmul(X[:, None, :, :].T, K.T).T.squeeze(1)
+
+
 if __name__ == "__main__":
 
-    n_training_iterations = 5000
-    n_experiments = 1
-    estimation_mode = "minimize f residual"
+    n_training_iterations = 1000000
+    lr = 1e-3
+    lr_schedule = True
+    n_experiments = 100
+    n_modes = 9  # set to None for no reduction
+    estimation_mode = "pressure rhs"
+
+    if n_modes is not None:
+        assert (
+            n_experiments >= n_modes
+        ), "the number of modes can not exceed the number of experiment"
 
     # =================== load data ===================
 
@@ -204,28 +185,56 @@ if __name__ == "__main__":
             samples=n_experiments,
         )
     )
-    permeabilities[0] = torch.tensor(
-        [1.0, 0.0005, 1.0, 1.0, 0.0005, 1.0, 1.0, 1.0, 1.0]
-    )
+    # permeabilities[0] = torch.tensor(
+    #     [1.0, 0.0005, 1.0, 1.0, 0.0005, 1.0, 1.0, 1.0, 1.0]
+    # )
 
-    pressures = darcy_block_solver(Ks, permeabilities, rhs)
-    p_with_outlet = add_outlet_to_solution(pressures, xy_coords)
+    K, pressures = darcy_block_solver(Ks, permeabilities, rhs)
+
+    # =================== dimensionality reduction =============
+    if n_modes is not None:
+        U, S, V = torch.linalg.svd(pressures, full_matrices=False)
+
+        z = U.T @ pressures
+        p_without_outlet_reconstructed_full = U @ z
+
+        Ut = U[:, :n_modes]
+        zt = Ut.T @ pressures
+        p_reconstructed_truncated = Ut @ zt
+        K_reduced = Ut.T.cpu() @ K.to_dense() @ Ut.cpu()
+
+        p_train = zt
+        rhs_train = Ut.T @ rhs
+    else:
+        p_train = pressures
+        rhs_train = rhs
 
     # =================== train ===================
 
     K, losses = train(
-        pressures, rhs, n_training_iterations, estimation_mode=estimation_mode
+        p_train,
+        rhs_train,
+        n_training_iterations,
+        estimation_mode=estimation_mode,
+        lr=lr,
+        lr_schedule=lr_schedule,
     )
 
     if estimation_mode == "pressure rhs":
-        p_estimated = (K @ rhs).detach().cpu()
+        p_estimated = (K @ rhs_train).detach().cpu()
     elif estimation_mode in {"pressure lhs", "minimize f residual"}:
-        p_estimated = torch.linalg.solve(K, rhs).detach().cpu()
+        p_estimated = torch.linalg.solve(K, rhs_train).detach().cpu()
     else:
         raise ValueError()
 
-    p_estimated_with_outlet = add_outlet_to_solution(p_estimated, xy_coords)
+    # ================== project solution to full order space ==================
 
+    if n_modes is not None:
+        p_estimated = Ut @ p_estimated
+
+    # ================== add outlet to solution =======================
+    p_estimated_with_outlet = add_outlet_to_solution(p_estimated, xy_coords)
+    p_with_outlet = add_outlet_to_solution(pressures, xy_coords)
     K = K.detach().cpu()
 
     # =================== plotting ===================
